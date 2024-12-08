06bb1ff29fb17ef77ceef89b0e0e1774524b1220
G-Desktop <laparkaes@gmail.com>
2024-12-08 01:53:02 -0500

    .

--- application/controllers/Load_bvl.php
+++ application/controllers/Load_bvl.php
@@ -44,7 +44,7 @@ class Load_bvl extends CI_Controller {
 		echo number_format($qty_new)." new companies created. (".number_format(microtime(true) - $this->start_time, 2)." sec)";
 	}
 	
-	public function last_stock(){
+	public function today(){
 		$url = "https://dataondemand.bvl.com.pe/v1/stock-quote/market";
 		$data = [
 			"companyCode" => "",
@@ -58,12 +58,47 @@ class Load_bvl extends CI_Controller {
 		$qty_up = $res->up;
 		$qty_down = $res->down;
 		$qty_equal = $res->equal;
-		
 		$stocks = $res->content;
 		foreach($stocks as $item){
-			print_r($item);
-			echo "<br/><br/>";
+			
+			$stock = [
+				'code' => property_exists($item, 'companyCode') ? $item->companyCode : null,
+				'name' => property_exists($item, 'companyName') ? $item->companyName : null,
+				'name_short' => property_exists($item, 'shortName') ? $item->shortName : null,
+				'sector_code' => property_exists($item, 'sectorCode') ? $item->sectorCode : null,
+				'sector_desc' => property_exists($item, 'sectorDescription') ? $item->sectorDescription : null,
+				'nemonico' => property_exists($item, 'nemonico') ? $item->nemonico : null,
+				'date_last' => property_exists($item, 'lastDate') ? $item->lastDate : null,
+				'date_created' => property_exists($item, 'createdDate') ? $item->createdDate : null,
+				'date_previous' => property_exists($item, 'previousDate') ? $item->previousDate : null,
+				'previous' => property_exists($item, 'previous') ? $item->previous : null,
+				'buy' => property_exists($item, 'buy') ? $item->buy : null,
+				'sell' => property_exists($item, 'sell') ? $item->sell : null,
+				'minimun' => property_exists($item, 'minimun') ? $item->minimun : null,
+				'maximun' => property_exists($item, 'maximun') ? $item->maximun : null,
+				'open' => property_exists($item, 'opening') ? $item->opening : null,
+				'close' => property_exists($item, 'last') ? $item->last : null,
+				'variation_per' => property_exists($item, 'percentageChange') ? $item->percentageChange : null,
+				'currency' => property_exists($item, 'currency') ? $item->currency : null,
+				'nego_qty' => property_exists($item, 'negotiatedQuantity') ? $item->negotiatedQuantity : null,
+				'nego_amount' => property_exists($item, 'negotiatedAmount') ? $item->negotiatedAmount : null,
+				'nego_amount_pen' => property_exists($item, 'negotiatedNationalAmount') ? $item->negotiatedNationalAmount : null,
+				'num_operation' => property_exists($item, 'operationsNumber') ? $item->operationsNumber : null,
+				'num_nego' => property_exists($item, 'numNeg') ? $item->numNeg : null,
+				'exderecho' => property_exists($item, 'exderecho') ? $item->exderecho : null,
+				'unity' => property_exists($item, 'unity') ? $item->unity : null,
+				'segment' => property_exists($item, 'segment') ? $item->segment : null,
+			];
+			
+			if ($stock['open']){
+			
+			
+				print_r($stock);
+				echo "<br/><br/>";	
+			}
+			
 		}
+		
 	}
 
 	public function index(){
